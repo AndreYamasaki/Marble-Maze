6af05fdd7f411214c0e01bfe2dbe992661f0f6fd
Andre <katsuya.tsu@gmail.com>
2021-08-12 12:19:35 -0300

    Initial Commit

---
+++ Project26/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  Project26
+//
+//  Created by user on 12/08/21.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
